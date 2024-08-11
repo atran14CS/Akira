5cfc7f804aa56e4d0fe68aa0eec0291c3d0fad05
atran14CS <atran14@uw.edu>
2024-08-11 00:26:24 -0700

    relative to abs and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'atran14CS.git.io', // Replace 'repository-name' with your actual GitHub repo name
   plugins: [react()],
 })
